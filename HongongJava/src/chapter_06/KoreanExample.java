e11a94aa0e6b9efc59e99a362d0ea38f2d55e5d6
dayul <dyulchoo@naver.com>
2024-01-22 11:43:12 +0900

    생성자에서 필드 초기화, this. 사용 실습

---
+++ HongongJava/src/chapter_06/KoreanExample.java
@@ -0,0 +1,12 @@
+package chapter_06;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		
+		Korean k1 = new Korean("박자바", "0112121-432342");
+		System.out.println(k1.nation + " "+ k1.name + " " + k1.ssn);
+
+	}
+
+}
