189795e89dcbbf3d2472b2693ed2370be90a8e18
dayul <dyulchoo@naver.com>
2024-01-22 23:55:05 +0900

    매개변수의 개수를 모를때 해결책 실습

---
+++ HongongJava/src/chapter_06/Computer.java
@@ -0,0 +1,20 @@
+package chapter_06;
+
+public class Computer {
+	// 둘다 같은 값을 리턴함
+	int sum1(int values[]) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	int sum2(int ... values) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+}
