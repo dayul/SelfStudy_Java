cd86fcc6531cae2a66a71f9770ff5ddde03b2e2f
dayul <dyulchoo@naver.com>
2024-01-23 11:44:37 +0900

    클래스 내부, 외부에서 메소드 호출

--- HongongJava/src/chapter_06/CalculatorExample.java
+++ HongongJava/src/chapter_06/CalculatorExample.java
@@ -7,13 +7,7 @@ public class CalculatorExample {
 		Calculator myCalc = new Calculator();
 		myCalc.powerOn();
 		
-		int plus = myCalc.plus(3,5);
-		System.out.println(plus);
-		
-		byte x = 10;
-		byte y = 4;
-		double divide = myCalc.divide(x, y);
-		System.out.println(divide);
+		myCalc.execute();
 		
 		myCalc.powerOff();
 
