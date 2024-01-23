55c6f911820b304164aafd0d3a14eec579e383c0
dayul <dyulchoo@naver.com>
2024-01-23 11:58:01 +0900

    메소드 오버로딩

---
+++ HongongJava/src/chapter_06/Calculator1.java
@@ -0,0 +1,14 @@
+package chapter_06;
+
+public class Calculator1 {
+	// 메소드 오버로딩
+	// 정사각형의 넓이
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	// 직사각형의 넓이
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+}
