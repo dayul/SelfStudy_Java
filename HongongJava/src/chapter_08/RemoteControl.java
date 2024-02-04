47e8576c4179c8daa97a91b11ff1402e5736c275
dayul <dyulchoo@naver.com>
2024-02-04 23:39:23 +0900

    인터페이스 선언 예제

---
+++ HongongJava/src/chapter_08/RemoteControl.java
@@ -0,0 +1,14 @@
+package chapter_08;
+
+public interface RemoteControl {
+	
+	// 상수 필드
+	public int MAX_VOLUME = 10;
+	public int MIN_VOLUME = 0;
+	
+	// 추상 메소드 - 메소드 선언부만 작성
+	public void turnOn();
+	public void turnOff();
+	public void setVolume(int volume);
+	
+}
